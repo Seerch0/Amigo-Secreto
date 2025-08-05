ce12124338367cffee121516981118a818ab65e7
Sergio Mendoza <sergiomendoza.ar@gmail.com>
2025-08-05 11:43:34 -0400

    Agrega la función de agregar nombres en apps.js

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 //Practica 1
 let nombres = [];
+
+function agregarNombre() {
+    let nombre = document.getElementById("nombreInput").value;
+    if (nombre) {
+        nombres.push(nombre);
+        document.getElementById("nombreInput").value = "";
+        mostrarNombres();
+    }
+}
